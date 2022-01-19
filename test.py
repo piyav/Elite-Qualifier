f839cb79b7042bbbd09b7f56c686463f3de49f21
Piya Verma <97057301+piyav@users.noreply.github.com>
2022-01-19 23:53:20 +0000

    added a test.py file

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import displayData
+
+
+class TestDisplayFata(unittest.TestCase):
+
+    def test_display_data(self):
+      self.assertTrue
+
+if __name__ == '__main__':
+    unittest.main()
